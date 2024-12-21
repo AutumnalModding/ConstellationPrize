b9663dd92f38a8905f997cd6de4c0445de5faabd
Lily Autumn <realprismaticyt@gmail.com>
2024-12-21 20:05:42 +1300

    Who up controlling they codes

--- src/main/java/xyz/lilyflower/conpri/client/renderer/module/neural/ModuleNeuralDialogue.java
+++ src/main/java/xyz/lilyflower/conpri/client/renderer/module/neural/ModuleNeuralDialogue.java
@@ -4,15 +4,18 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.ColorHelper;
+import xyz.lilyflower.conpri.client.renderer.Display;
 import xyz.lilyflower.conpri.client.renderer.Display.Constants;
 import xyz.lilyflower.conpri.client.renderer.module.RendererModule;
 import xyz.lilyflower.conpri.util.TextRenderer;
 
 @SuppressWarnings("unused")
 public class ModuleNeuralDialogue implements RendererModule {
+    public static String CURRENT_PORTRAIT = "";
+
     @Override
     public boolean shouldRender() {
-        return false;
+        return Display.GLASSES_EQUIPPED;
     }
 
     @Override
@@ -22,13 +25,28 @@ public class ModuleNeuralDialogue implements RendererModule {
 
     @Override
     public void renderDebug(DrawContext context, RenderTickCounter counter) {
+        if (!TextRenderer.SHOULD_UPDATE) {
+            TextRenderer.DRAW_POSITION_X = Display.Constants.PORTRAIT_X + Display.Constants.PORTRAIT_SIZE + 6;
+            TextRenderer.DRAW_POSITION_Y = Display.Constants.PORTRAIT_Y + 1;
+
+            TextRenderer.init(new TextRenderer.Message(
+                    "textures/gui/portrait/pancakes.png",
+                    null,
+                    null,
+                    "This is some text. It's fourty characters!",
+                    "And you can have up to six lines, too!",
+                    "You can even\000\001\001\144 pause text while parsing!",
+                    "Or change the\000\001\002\012 line speed \000\001\002\002mid-line!\000\001\001\062 "
+            ));
+        }
+
         context.fill(Constants.BORDER_PADDING, Constants.BORDER_PADDING, Constants.BORDER_WIDTH, Constants.BORDER_HEIGHT, 0, ColorHelper.Argb.getArgb(0x70, 0x00, 0x00));
         context.fill(Constants.DIALOGUE_BOX_X, Constants.DIALOGUE_BOX_Y, Constants.DIALOGUE_BOX_WIDTH, Constants.DIALOGUE_BOX_HEIGHT, 0, ColorHelper.Argb.getArgb(0x20, 0x20, 0x20));
         context.fill(Constants.DIALOGUE_BOX_WIDTH, Constants.DIALOGUE_BOX_Y, Constants.DIALOGUE_BOX_WIDTH - Constants.DIALOGUE_BOX_HEIGHT, Constants.DIALOGUE_BOX_HEIGHT, 0, ColorHelper.Argb.getArgb(0x00, 0xFF, 0xFF));
 
         context.fill(Constants.PORTRAIT_X - 4, Constants.PORTRAIT_Y - 4, Constants.PORTRAIT_X + Constants.PORTRAIT_SIZE + 4, Constants.PORTRAIT_Y + Constants.PORTRAIT_SIZE + 4, 0, ColorHelper.Argb.getArgb(0x00, 0x00, 0xFF));
         context.fill(Constants.PORTRAIT_X, Constants.PORTRAIT_Y, Constants.PORTRAIT_X + Constants.PORTRAIT_SIZE, Constants.PORTRAIT_Y + Constants.PORTRAIT_SIZE, 0, ColorHelper.Argb.getArgb(0x00, 0xFF, 0x00));
-        context.drawTexture(Identifier.of("conpri", "textures/gui/portrait/pancakes.png"), Constants.PORTRAIT_X, Constants.PORTRAIT_Y, 0, 0, 0, Constants.PORTRAIT_SIZE, Constants.PORTRAIT_SIZE, Constants.PORTRAIT_SIZE, Constants.PORTRAIT_SIZE);
+        context.drawTexture(Identifier.of("conpri", CURRENT_PORTRAIT), Constants.PORTRAIT_X, Constants.PORTRAIT_Y, 0, 0, 0, Constants.PORTRAIT_SIZE, Constants.PORTRAIT_SIZE, Constants.PORTRAIT_SIZE, Constants.PORTRAIT_SIZE);
 
         TextRenderer.update(context);
     }
--- src/main/java/xyz/lilyflower/conpri/util/TextRenderer.java
+++ src/main/java/xyz/lilyflower/conpri/util/TextRenderer.java
@@ -2,6 +2,11 @@ package xyz.lilyflower.conpri.util;
 
 import java.util.ArrayList;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.sound.SoundEvent;
+import xyz.lilyflower.conpri.client.renderer.module.neural.ModuleNeuralDialogue;
+import xyz.lilyflower.conpri.init.ConstellationPrize;
+import xyz.lilyflower.conpri.init.ConstellationPrizeClient;
 
 public class TextRenderer {
     public static int LINE_COUNT = 0;
@@ -15,48 +20,97 @@ public class TextRenderer {
     public static int DRAW_POSITION_X = 0;
     public static int DRAW_POSITION_Y = 0;
 
-    public static boolean SHOULD_UPDATE = true;
+    public static boolean IS_PAUSED = false;
+    public static int PAUSE_DURATION_MAX = 0;
+    public static int PAUSE_DURATION_ELAPSED = 0;
+
+    public static boolean SHOULD_UPDATE = false;
     public static net.minecraft.client.font.TextRenderer RENDERER_INSTANCE;
+    public static Message CURRENT_MESSAGE;
 
-    public static void init(String... lines) {
-        LINE_ARRAY = new StringBuilder[lines.length];
-        LINE_COUNT = lines.length;
+    public static void init(Message message) {
+        LINE_ARRAY = new StringBuilder[message.lines.length];
+        LINE_COUNT = message.lines.length;
 
-        for (int line = 0; line < lines.length; line++) {
+        for (int line = 0; line < message.lines.length; line++) {
             LINE_ARRAY[line] = new StringBuilder();
 
-            char[] text = new char[lines[line].length()];
+            char[] text = new char[message.lines[line].length()];
             for (int i = 0; i < text.length; i++) {
-                text[i] = lines[line].charAt(i);
+                text[i] = message.lines[line].charAt(i);
             }
 
             LINE_CONTENT.add(text);
         }
-    }
 
+        if (message.voiceline != null) {
+            ConstellationPrizeClient.CLIENT_INSTANCE.getSoundManager().play(PositionedSoundInstance.master(message.voiceline, 1.0F, 1.0F));
+        }
+
+        SHOULD_UPDATE = true;
+        CURRENT_MESSAGE = message;
+        ModuleNeuralDialogue.CURRENT_PORTRAIT = message.portrait;
+    }
+    
     public static void update(DrawContext context) {
         LINE_DELTA++;
 
+        for (int line = 0; line <= LINE_INDEX; line++) {
+            context.drawText(RENDERER_INSTANCE, LINE_ARRAY[line].toString(), DRAW_POSITION_X, DRAW_POSITION_Y + (line * 11), 0xFFFFFF, true);
+        }
+
         if (SHOULD_UPDATE) {
-            for (int line = 0; line <= LINE_INDEX; line++) {
-                context.drawText(RENDERER_INSTANCE, LINE_ARRAY[line].toString(), DRAW_POSITION_X, DRAW_POSITION_Y + (line * 11), 0xFFFFFF, true);
+            if (IS_PAUSED) {
+                PAUSE_DURATION_ELAPSED++;
+
+                if (PAUSE_DURATION_ELAPSED >= PAUSE_DURATION_MAX) {
+                    IS_PAUSED = false;
+                    PAUSE_DURATION_MAX = 0;
+                    PAUSE_DURATION_ELAPSED = 0;
+                } else {
+                    return;
+                }
             }
 
             if (LINE_DELTA % LINE_SPEED == 0) {
+                if (CURRENT_MESSAGE.talksound != null) {
+                    ConstellationPrizeClient.CLIENT_INSTANCE.getSoundManager().play(PositionedSoundInstance.master(CURRENT_MESSAGE.talksound, 1.0F, 1.0F));
+                }
+
+                char next = LINE_CONTENT.get(LINE_INDEX)[LINE_POSITION];
+
+                if (next == 0x00) {
+                    char header_lower = LINE_CONTENT.get(LINE_INDEX)[++LINE_POSITION];
+                    char header_upper = LINE_CONTENT.get(LINE_INDEX)[++LINE_POSITION];
+
+                    for (RendererCommand command : RendererCommand.values()) {
+                        if (command.header[0] == header_lower && command.header[1] == header_upper) {
+                            char[] arguments = new char[command.arguments];
+                            for (int arg = 0; arg < arguments.length; arg++) {
+                                arguments[arg] = LINE_CONTENT.get(LINE_INDEX)[++LINE_POSITION];
+                            }
+                            command.executor.run(arguments);
+
+                            break;
+                        }
+                    }
+                } else {
+                    LINE_ARRAY[LINE_INDEX].append(next);
+                }
+
                 LINE_POSITION++;
 
                 if (LINE_POSITION >= LINE_CONTENT.get(LINE_INDEX).length) {
                     LINE_POSITION = 0;
                     LINE_INDEX++;
 
-                    if (LINE_INDEX > LINE_COUNT) {
+                    if (LINE_INDEX >= LINE_COUNT) {
                         LINE_INDEX = 0;
                         LINE_CONTENT.clear();
 
                         for (int index = 0; index < LINE_ARRAY.length; index++) {
                             LINE_ARRAY[index] = new StringBuilder();
                         }
-
                         SHOULD_UPDATE = false;
                     }
                 }
@@ -64,30 +118,45 @@ public class TextRenderer {
         }
     }
 
-    public static void parseCommand(int command) {
-        switch (command) {
-            case RendererCommand.END_LINE -> {} // TODO Implement waiting for input at the end of a line
-            case RendererCommand.END_TEXT_BLOCK -> {} // TODO Implement waiting for input at the end of a text block
-            case RendererCommand.LINE_SPEED_FAST -> LINE_SPEED = LineSpeed.FAST;
-            case RendererCommand.LINE_SPEED_MEDIUM -> LINE_SPEED = LineSpeed.MEDIUM;
-            case RendererCommand.LINE_SPEED_SLOW -> LINE_SPEED = LineSpeed.SLOW;
-            case RendererCommand.LINE_SPEED_SNAIL -> LINE_SPEED = LineSpeed.SNAIL;
+    public static class LineSpeed {
+        public static final int FAST = 2;
+        public static final int MEDIUM = 4;
+        public static final int SLOW = 6;
+        public static final int SNAIL = 8;
+        public static final int VISCOUS = 10;
+    }
+
+    public enum RendererCommand {
+        WAIT_FOR_INPUT(new char[]{0x01, 0x00}, 0, arguments -> {
+            // NOP
+        }),
+
+        PAUSE_FOR_FRAMES(new char[]{0x01, 0x01}, 1, arguments -> {
+            PAUSE_DURATION_MAX = arguments[0];
+            IS_PAUSED = true;
+        }),
+
+        SET_LINE_SPEED(new char[]{0x01, 0x02}, 1, arguments -> {
+            LINE_SPEED = arguments[0] == 0x00 ? 0 : arguments[0]; // TODO: line speed config
+        })
+
+        ;
+
+        private final char[] header;
+        private final int arguments;
+        private final Executor executor;
+
+        RendererCommand(char[] header, int arguments, Executor executor) {
+            this.header = header;
+            this.arguments = arguments;
+            this.executor = executor;
+        }
+
+        @FunctionalInterface
+        public interface Executor {
+            void run(char... arguments);
         }
     }
 
-    public static class LineSpeed {
-        public static final int FAST = 1;
-        public static final int MEDIUM = 2;
-        public static final int SLOW = 3;
-        public static final int SNAIL = 4;
-    }
-
-    public static class RendererCommand {
-        public static final int END_LINE = 0x3;
-        public static final int END_TEXT_BLOCK = 0x4;
-        public static final int LINE_SPEED_FAST = 0x11;
-        public static final int LINE_SPEED_MEDIUM = 0x12;
-        public static final int LINE_SPEED_SLOW = 0x13;
-        public static final int LINE_SPEED_SNAIL = 0x14;
-    }
+    public record Message(String portrait, SoundEvent voiceline, SoundEvent talksound, String... lines) {}
 }
